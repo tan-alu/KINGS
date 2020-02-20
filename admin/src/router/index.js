2db10e50fbd4163201b6ab99204efffc84ecd0ef
tan-alu <786894966@qq.com>
2020-02-20 16:46:11 +0800

    创建分类【客户端】

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -1,17 +1,19 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-// import Home from '../views/Home.vue'
 import Main from '../views/Main.vue'
+import CategoryEdit from '../views/CategoryEdit.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'main',
-    component: Main
-  }
-]
+const routes = [{
+  path: '/',
+  name: 'main',
+  component: Main,
+  children: [{
+    path: '/categories/create',
+    component: CategoryEdit
+  }]
+}]
 
 const router = new VueRouter({
   routes
