f55e3d2fe5fda8f75f88656bfcf0f0f6a18dce0d
tan-alu <786894966@qq.com>
2020-03-20 11:59:58 +0800

    上传文件的修复（注释掉了，）

--- admin/src/main.js
+++ admin/src/main.js
@@ -9,6 +9,21 @@ import './style.css'
 Vue.config.productionTip = false
 Vue.prototype.$http = http
 
+// Vue.mixin({
+//   computed: {
+//     upload() {
+//       return this.$http.defaults.baseURL + '/upload'
+//     }
+//   },
+//   methods: {
+//     getAuthHeaders() {
+//       return {
+//         Authorization: `Bearer ${localStorage.token || ''}`
+//       }
+//     }
+//   }
+// })
+
 new Vue({
   router,
   store,
--- admin/src/views/ItemEdit.vue
+++ admin/src/views/ItemEdit.vue
@@ -9,6 +9,7 @@
       <el-form-item label="图标">
         <el-upload class="avatar-uploader" :action="$http.defaults.baseURL+'/upload'" :show-file-list="false"
           :on-success="afterUpload">
+          <!-- :headers='getAuthHeaders()' -->
           <img v-if="model.icon" :src="model.icon" class="avatar">
           <i v-else class="el-icon-plus avatar-uploader-icon"></i>
         </el-upload>
