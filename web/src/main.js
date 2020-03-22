bdc3700c44eba52b9ac55d626df4ad3e5f3d102a
tan-alu <786894966@qq.com>
2020-03-22 22:20:06 +0800

    主页框架和顶部菜单

--- web/src/main.js
+++ web/src/main.js
@@ -3,8 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './style.scss'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/css/swiper.css'
 
 Vue.config.productionTip = false
+Vue.use(VueAwesomeSwiper)
 
 new Vue({
   router,
